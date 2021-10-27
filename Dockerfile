96c6dcae972882eca02465bfec57e2a5a1afb86c
lindanimoyo <lindani@praelexis.com>
2021-10-27 06:31:13 +0200

    Make app simpler.

--- Dockerfile
+++ Dockerfile
@@ -3,7 +3,7 @@ FROM python:3.8-slim
 # Set default execution directory
 WORKDIR /model-api
 
-RUN pip install pandas scikit-learn flask gunicorn
+RUN pip install pandas scikit-learn==1.0.1 flask gunicorn
 
 # Copy rest of the project and install
 COPY . .
--- model_api/app.py
+++ model_api/app.py
@@ -4,45 +4,39 @@
 from flask import Flask, jsonify, request
 import pandas
 import numpy
+import joblib
 
-# Local imports
-from model_api.model import ModelApi
-from model_api import MODEL_DIR
 
 # Load model
-MODEL = ModelApi()
-MODEL.load(MODEL_DIR)
+model = joblib.load('./model/PVFaultClassification_RandomForestModel')
 
 app = Flask(__name__)
 
 
-@app.route("/predict")
+@app.route('/predict',methods=['POST'])
 def predict():
     """
     Handle a request to the predict end-point.
     """
 
-    req_body = request.get_json(silent=True)
+    request_body = request.get_json(silent=True)
 
     # convert input data into dataframe
-    names = numpy.array(req_body['data']['names'])
-    input_arr = numpy.array(req_body['data']['ndarray'])
+    cols = numpy.array(request_body['data']['columns'])
+    arr = numpy.array(request_body['data']['values'])
 
     # run prediction
-    query_df = pandas.DataFrame(data=input_arr, columns=names)
-    predictions = MODEL.predict(query_df)
-    p_packged = [[p] for p in predictions]
+    query_df = pandas.DataFrame(data=arr, columns=cols)
+    predictions = model.predict(query_df)
 
     return jsonify({
         "status": {
             "code": 200,
             "status": "SUCCESS"
         },
-        "data": {
-            "ndarray": p_packged
-        }
+        "result": str(predictions[0])
     })
 
 
 if __name__ == '__main__':  # pragma: no cover
-    app.run(host='0.0.0.0', port=8080, debug=False)
+    app.run(debug=True)
